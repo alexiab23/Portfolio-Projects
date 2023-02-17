fa1ac08c5a999493a0a96abde980ead36eb86874
Alexia Brown <110739017+alexiab23@users.noreply.github.com>
2023-02-16 21:05:16 -0500

    update

--- Python Band Name Generator.py
+++ Python Band Name Generator.py
@@ -1,14 +1,14 @@
---start off with a greeting message
+#start off with a greeting message
 print("Hello" + "" + input("What is your name?") + " " + "thank you for using our Band Name generator!")
 
---Gather questions for the user to interact with 
+#Gather questions for the user to interact with 
 city = input("What amazing city did you grow up in?\n")
 pet = input("What is/will be the name of your lovely pet?\n")
 color = input("What is your favorite color?\n")
 vegetable = input("What is your favorite vegetable?\n")
 fruit = input("What is your favorite fruit?\n") 
 
---Put everything together and create different combinations
+#Put everything together and create different combinations
 print("Your band name could be:")
 print(city + " " + pet)
 print(city + " " + color)
@@ -23,7 +23,7 @@ print(city + " " + pet)
 print(pet + " " + fruit)
 print(fruit + " " + color)
 
--- The full code below is:
+#The full code below is:
 
 print("Hello" + " " + input("What is your name?") + " " + "thank you for using our Band Name generator!")
 city = input("What amazing city did you grow up in?\n")
